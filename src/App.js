50b722d32b6de08cd59885f361151b06e00beb04
Rafaella Keury <Rafaella_Souza@gap.com>
2020-01-21 14:54:44 -0300

    Update app with ShopPage and CollectionItem

--- src/App.js
+++ src/App.js
@@ -2,19 +2,15 @@ import React from 'react';
 import './App.css';
 import { Switch, Route} from 'react-router-dom';
 import HomePage from './pages/homepage/HomePage';
+import ShopPage from './pages/shop/ShopPage';
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
